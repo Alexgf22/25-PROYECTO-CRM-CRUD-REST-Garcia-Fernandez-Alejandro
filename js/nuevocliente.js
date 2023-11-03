13f2dc6dc6134dee7842dda526417cff8fa119d0
Alexgf22 <agarcia@iesrafaelalberti.es>
2023-11-03 21:39:04 +0100

    Solucion problema cliente al array copia de object

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -1,6 +1,6 @@
 var listadoClientes = []
 
-// Objeto con el contenido del mensaje
+// Objeto con los datos del cliente
 let clienteOBJ = {
     nombre: "",
     email: "",
@@ -8,13 +8,9 @@ let clienteOBJ = {
     empresa: ""
 }
 
-
 // Selectores y Listeners
-
 document.addEventListener("DOMContentLoaded", () => {
 
-
-    
     // Selectores
     const inputNombre = document.querySelector("#nombre")
     const inputCorreo = document.querySelector("#email")
@@ -22,46 +18,23 @@ document.addEventListener("DOMContentLoaded", () => {
     const inputEmpresa = document.querySelector("#empresa")
     const formulario = document.querySelector("#formulario")
     const btnSubmit = document.querySelector('#formulario button[type = "submit"]')
-    //const btnReset = document.querySelector('#formulario button[type = "reset"]')
     const spinner = document.querySelector("#spinner")
     var tablaDeClientes = document.querySelector("#listado-clientes")
-
 
     // Deshabilitar el botón por defecto
     btnSubmit.disabled = true
     btnSubmit.classList.add("opacity-50")
 
     // Listeners
-
-    // Listener para el botón de añadir
     btnSubmit.addEventListener("click", () => {
     anadirHTML()
     })
 
-    // Listener para remover clientes
-    /*
-    tablaDeClientes.addEventListener("click", (e) => {
-        if (e.target.className == "borrar-mensaje") {
-        var indiceLi = parseInt(e.target.parentElement.dataset.indice)
-        // Eliminamos el mensaje del array
-        listadoClientes.splice(indiceLi, 1)
-        // Actualizamos el localStorage tras haber eliminado el mensaje
-        localStorage.setItem("Clientes", JSON.stringify(listadoClientes))
-        console.log(localStorage)
-        // Eliminamos el 'li' del mensaje correspondiente
-        e.target.parentElement.remove()
-        }
-    })
-    */
-
-
-    // Añadir los listeners para resaltar campo activo
     inputNombre.addEventListener("focus", resaltarCampoActivo)
     inputCorreo.addEventListener("focus", resaltarCampoActivo)
     inputTelefono.addEventListener("focus", resaltarCampoActivo)
     inputEmpresa.addEventListener("focus", resaltarCampoActivo)
 
-    // Si en vez de blur uso input me lo valida sobre la marcha
     inputNombre.addEventListener("blur", (e) => {
         quitarResaltadoCampo(e)
         validar(e)
@@ -79,42 +52,14 @@ document.addEventListener("DOMContentLoaded", () => {
         validar(e)
     })
     formulario.addEventListener("submit", activarSpinner)
-    /* btnReset.addEventListener("click", (e) => {
-        e.preventDefault()
-        resetForm()
-    }) */
-
     
-
-
     // Funciones
-
-    
-  
-
-    // Se agrega un nuevo cliente a la tabla de clientes en el HTML
     function anadirHTML() {
-
-        // Creamos un nuevo objeto cliente para cada cliente que se añade
-        clienteOBJ = {
-            nombre: inputNombre.value,
-            email: inputCorreo.value,
-            telefono: inputTelefono.value,
-            empresa: inputEmpresa.value
-        }
-
-       
-        //console.log(clienteOBJ.nombre)
-        
         const fila = document.createElement("tr")
     
         const nombreCliente = document.createElement("td")
         nombreCliente.textContent = clienteOBJ.nombre
         fila.appendChild(nombreCliente)
-    
-        /* const emailCliente = document.createElement("td")
-        emailCliente.textContent = clienteOBJ.email
-        fila.appendChild(emailCliente) */
     
         const telefonoCliente = document.createElement("td")
         telefonoCliente.textContent = clienteOBJ.telefono
@@ -123,7 +68,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const empresaCliente = document.createElement("td")
         empresaCliente.textContent = clienteOBJ.empresa
         fila.appendChild(empresaCliente)
-    
+
         const acciones = document.createElement("td")
 
         const contenedorBotones = document.createElement("div")
@@ -132,7 +77,6 @@ document.addEventListener("DOMContentLoaded", () => {
         botonEditar.textContent = "Editar Cliente"
 
         botonEditar.classList.add("bg-teal-600")
-        //botonEditar.classList.add("w-full")
         botonEditar.classList.add("mt-5")
         botonEditar.classList.add("p-2")
         botonEditar.classList.add("text-white")
@@ -150,14 +94,12 @@ document.addEventListener("DOMContentLoaded", () => {
             window.location.href = `editar-cliente.html?nombre=${nombre}&email=${email}&telefono=${telefono}&empresa=${empresa}`;
         })
 
-        
         const espacio = document.createTextNode(" ")
     
         const botonBorrar = document.createElement("button")
         botonBorrar.textContent = "Borrar Cliente"
 
         botonBorrar.classList.add("bg-teal-600")
-        //botonBorrar.classList.add("w-full")
         botonBorrar.classList.add("mt-5")
         botonBorrar.classList.add("p-2")
         botonBorrar.classList.add("text-white")
@@ -165,18 +107,13 @@ document.addEventListener("DOMContentLoaded", () => {
         botonBorrar.classList.add("font-bold")
 
         botonBorrar.addEventListener("click", (e) => {
-            // Se busca la fila donde se haya presionado el botón
             const fila = e.target.parentElement.parentElement.parentElement
-            /* Le restamos 1 al índice para poder buscar en el array
-             ya que en la tabla, el índice comienza en 1 y en el array
-             en 0
-            */
             const indiceFila = fila.rowIndex - 1
 
             // Eliminamos el cliente de la lista y del DOM
             listadoClientes.splice(indiceFila, 1)
             fila.remove()
-        })
+        }) 
 
         contenedorBotones.appendChild(botonEditar)
         contenedorBotones.appendChild(espacio)
@@ -188,35 +125,22 @@ document.addEventListener("DOMContentLoaded", () => {
     
         tablaDeClientes.appendChild(fila)
 
-        console.log(clienteOBJ)
-    
-        // Agregamos los datos del cliente a la lista de clientes
-        listadoClientes = [...listadoClientes, clienteOBJ]
-        console.log("Listado de clientes:", listadoClientes)
-    
-        // Restablecemos los valores del objeto cliente por defecto
-        clienteOBJ.nombre = ""
-        clienteOBJ.email = ""
-        clienteOBJ.telefono = ""
-        clienteOBJ.empresa = ""
+        let copiaClienteOBJ = { ...clienteOBJ }
 
+        // Agregamos los datos del cliente a la lista de clientes
+        listadoClientes.push(copiaClienteOBJ)
     }
 
-
-
-    // Resaltamos el campo activo
     function resaltarCampoActivo(e) {
         e.target.style.borderColor = "#3498db"
         e.target.style.boxShadow = "0 0 10px rgba(52, 152, 219, 0.7)"
     }
 
-    // Eliminamos el resaltado cuando el campo pierde el foco
     function quitarResaltadoCampo(e) {
         e.target.style.borderColor = ""
         e.target.style.boxShadow = ""
     }
     
-
     function activarSpinner(e) {
         e.preventDefault()
         spinner.classList.remove("hidden")
@@ -225,10 +149,8 @@ document.addEventListener("DOMContentLoaded", () => {
         setTimeout(() => {
             spinner.classList.add("hidden")
             spinner.classList.remove("flex")
-
             resetForm()
 
-            // Creamos una alerta para confirmar que todo está OK
             const alerta = document.createElement("p")
             alerta.classList.add("bg-green-500", "text-white", "text-center",
             "rounded-lg", "mt-10", "text-sm")
@@ -241,9 +163,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
         }, 3000)
 
-        
-
-        
     }
 
     function resetForm() {
@@ -253,7 +172,6 @@ document.addEventListener("DOMContentLoaded", () => {
         clienteOBJ.empresa = ""
         formulario.reset()
         comprobarFormulario()
-
     }
 
     function validar(e) {
@@ -279,9 +197,7 @@ document.addEventListener("DOMContentLoaded", () => {
         limpiarAlerta(e.target.parentElement)
 
         clienteOBJ[e.target.name] = capitalizarNombreCompleto(e.target.value.trim())
-        comprobarFormulario(clienteOBJ)
-        //console.log(clienteOBJ)
-        
+        comprobarFormulario(clienteOBJ)        
     }
 
     function capitalizarNombreCompleto(nombreCompleto) {
@@ -293,7 +209,6 @@ document.addEventListener("DOMContentLoaded", () => {
     function comprobarFormulario() {
         const values = Object.values(clienteOBJ)
     
-        // Verificar si hay algún campo vacío o no válido
         const campoVacio = values.includes("")
         const formularioValido = values.every(value => value !== "")
     
@@ -306,7 +221,6 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
     
-
     function limpiarAlerta(referencia) {
         const alerta = referencia.querySelector(".bg-red-600")
         if (alerta) {
@@ -317,12 +231,9 @@ document.addEventListener("DOMContentLoaded", () => {
     function mostrarAlerta(mensaje, referencia) {
 
         limpiarAlerta(referencia)
-
-        console.log("Hubo un error...")
         const error = document.createElement("p")
         error.textContent = mensaje
         error.classList.add("bg-red-600", "text-center", "text-white", "p-2")
-        console.log(error)
         referencia.appendChild(error)
     }
 
@@ -337,8 +248,5 @@ document.addEventListener("DOMContentLoaded", () => {
         resultado = regex.test(telefono)
         return resultado
     }
-
-
-
 
 })
